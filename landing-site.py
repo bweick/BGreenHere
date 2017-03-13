b25f9a94a20ebfb70c671ac9aa55e57ca8c1218a
bweick <brian@biometrixtech.com>
2017-03-13 12:03:40 +0800

    heroku configurations

--- landing-site.py
+++ landing-site.py
@@ -12,3 +12,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return app.send_static_file('index.html')
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
