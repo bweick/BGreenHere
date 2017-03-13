6d54ea7a6cf2bdf3356907e5736218c22f107539
bweick <brian@biometrixtech.com>
2017-03-13 12:30:37 +0800

    configure heroku server

--- landing-site.py
+++ landing-site.py
@@ -14,4 +14,5 @@ def hello_world():
     return app.send_static_file('index.html')
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port
